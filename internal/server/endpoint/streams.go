f234db9f1a27e0cb0ed00e2694bbe6e71db739e0
Leif Haberecht <81562562+Deskilling@users.noreply.github.com>
2026-05-11 01:40:53 +0200

    server - streams

--- internal/server/endpoint/streams.go
+++ internal/server/endpoint/streams.go
@@ -1,7 +1,12 @@
 package endpoint
 
 import (
+	"encoding/json"
 	"net/http"
+
+	"mfg-dl/internal/sites/model"
+
+	"charm.land/log/v2"
 )
 
 func HandleStreams(w http.ResponseWriter, r *http.Request) {
@@ -9,4 +14,25 @@ func HandleStreams(w http.ResponseWriter, r *http.Request) {
 		writeError(w, "method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
+
+	var episode model.Episode
+	err := json.NewDecoder(r.Body).Decode(&episode)
+	if err != nil {
+		writeError(w, "invalid request body: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	site := getSite(episode.Service)
+	if site == nil {
+		writeError(w, "service not found", http.StatusBadRequest)
+	}
+
+	streams, err := site.Streams(episode)
+	if err != nil {
+		log.Errorf("failed streams", "err", err)
+		writeError(w, "streams failed", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(streams)
 }
