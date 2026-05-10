eb35a8d1770c22aabdb89aa2f683d4b9fc1931b3
Leif Haberecht <81562562+Deskilling@users.noreply.github.com>
2026-05-11 01:29:48 +0200

    server - seasons

--- internal/server/endpoint/seasons.go
+++ internal/server/endpoint/seasons.go
@@ -1,7 +1,12 @@
 package endpoint
 
 import (
+	"encoding/json"
 	"net/http"
+
+	"mfg-dl/internal/sites/model"
+
+	"charm.land/log/v2"
 )
 
 func HandleSeasons(w http.ResponseWriter, r *http.Request) {
@@ -9,4 +14,26 @@ func HandleSeasons(w http.ResponseWriter, r *http.Request) {
 		writeError(w, "method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
+
+	var searchResult model.SearchResult
+
+	err := json.NewDecoder(r.Body).Decode(&searchResult)
+	if err != nil {
+		writeError(w, "invalid request body: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	site := getSite(searchResult.Service)
+	if site == nil {
+		writeError(w, "service not found", http.StatusBadRequest)
+	}
+
+	seasons, err := site.Seasons(searchResult)
+	if err != nil {
+		log.Errorf("failed seasons", "err", err)
+		writeError(w, "seasons failed", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(seasons)
 }
